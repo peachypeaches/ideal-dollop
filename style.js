176dd11bd01f1d6a86d54596845937a4166621a7
officialyankey <105379436+officialyankey@users.noreply.github.com>
2022-08-08 06:35:55 -0700

    i created a functional scroll-to-top button

--- style.js
+++ style.js
@@ -1,5 +1,15 @@
 const scrollBtn = document.querySelector(".scroll-to-top")
 
-scrollBtn.addEventListener("click", {} => {
-    document.documentElement.scrollTop = 0;
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 });
+
+document.addEventListener("scroll", (e) => {
+  if (document.documentElement.scrollTop <= 150) {
+    scrollBtn.style.display = "none";
+  } else {
+    scrollBtn.style.display = "block";
+  }
+});
+AdiahaNsima99
